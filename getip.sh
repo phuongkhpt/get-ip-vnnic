3f6821e2553c2a52c1b831a5d352cfee7a5e46c6
phuongkhpt <anhyeuemnhieu57@gmail.com>
2024-09-10 14:53:37 +0700

    Update getip.sh

--- getip.sh
+++ getip.sh
@@ -1,9 +1,7 @@
-#!/bin/bash
-
+#!/usr/bin/env bash
 curl -s --header 'User-Agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36' -L http://thongkeinternet.vn/jsp/vnix/danhba_ipasn.jsp -o output.txt
-
 if [[ $? -ne 0 ]]; then
-    echo "Error: Curl command failed"
+	echo "Error: Curl command failed"
 else
 	egrep -o '[0-9]+[.][0-9]+[.][0-9]+[.][0-9]+/[0-9]+' output.txt > ip-vnnic
 fi
