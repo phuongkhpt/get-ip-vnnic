32d76c90b324c8cb067a5b5cbfbef2a0950e9497
phuongkhpt <anhyeuemnhieu57@gmail.com>
2024-09-10 11:58:11 +0700

    Update getip.sh

--- getip.sh
+++ getip.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 curl -s --header 'User-Agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36' -L http://thongkeinternet.vn/jsp/vnix/danhba_ipasn.jsp -o output.txt
 
